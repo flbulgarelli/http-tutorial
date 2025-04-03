0d15361a0be7e246c7e955c9ab4b444c4a3c9bfb
Franco Leonardo Bulgarelli <franco@mumuki.org>
2025-04-03 11:35:39 -0300

    Agregando comentarios

--- custom-client/index.js
+++ custom-client/index.js
@@ -1,13 +1,17 @@
+// esto es una biblioteca para hacer pedidos HTTP
 const axios = require('axios');
 
 const API_URL = 'http://localhost:3000/prendas';
 
+// que es este async?
 async function fetchPrendas() {
   try {
+    // get es un mensaje del objeto axios (_es_ la biblioteca) que sirve para hacer una petición HTTP de tipo GET
+    // que es este await?
     const response = await axios.get(API_URL);
     console.log('Prendas:', response.data);
   } catch (error) {
-    console.error('Error fetching prendas:', error.message);
+    console.error('Error cargando las prendas:', error.message);
   }
 }
 
